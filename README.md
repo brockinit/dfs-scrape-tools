ef8951c807f23a556f849b88e9fbdfe90409d5ef
Brock Lanoza <brock@sudokrew.com>
2017-04-29 13:14:50 -1000

    Update __init__.py

--- README.md
+++ README.md
@@ -4,4 +4,17 @@ A collection of web scrapers for obtaining NFL stats and fantasy data
 
 ## Basic Usage
 
-* Run `python init.py`
+* `pip install dfs-scrape-tools`
+
+* By default, `dfs-scrape-tools` will write the files to the CWD
+```python
+import dfs-scrape-tools
+
+options = {
+    'years': [2011, 2015],
+    'weeks': [1, 4, 7],
+    'game': 'fd'
+}
+
+dfs-scrape-tools.roto_guru(options)
+```
--- dfs-scrape-tools/__init__.py
+++ dfs-scrape-tools/__init__.py
@@ -1,17 +1,5 @@
 from roto_guru import roto_guru_scraper
 
 
-def scraper(job, options=None):
-    scrapers = {
-        'roto_guru_scraper': roto_guru_scraper
-    }
-
-    scrape_job = scrapers[job]
-
-    if scrape_job:
-        return scrape_job()
-    else:
-        print('No job of this type available')
-
-
-scraper('roto_guru_scraper')
+def roto_guru(options):
+    return roto_guru_scraper(options.years, options.weeks, options.game)
