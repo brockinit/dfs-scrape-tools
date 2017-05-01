9a81caaf00a5f0c6b45ef4732e0f9ac58c4ccc43
Brock Lanoza <brock@sudokrew.com>
2017-04-30 16:46:44 -1000

    Add query params dynamically

--- interscraped/fantasy_data.py
+++ interscraped/fantasy_data.py
@@ -2,16 +2,22 @@ import os
 from robobrowser import RoboBrowser
 
 login_url = 'https://fantasydata.com/user/login.aspx'
-snap_count_url = 'https://fantasydata.com/nfl-stats/nfl-fantasy-football-snap-count-and-snaps-played.aspx?fs=0&stype=0&sn=0&scope=0&w=0&ew=0&s=&t=0&p=2&st=FantasyPointSnapPercentage&d=1&ls=&live=false&pid=true&minsnaps=4'
 
-default_years = [2011, 2012, 2013, 2014, 2015, 2016]
-default_weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+# 2012, 2013, 2014, 2015, 2016
+default_years = [0, 1, 2, 3, 4]
+
+default_weeks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 # QB, RB, WR, TE
 default_pos = [2, 3, 4, 5]
 
-headers = 'rank,id,player,pos,team,gms,snaps,snapsgm,snappct,rushpct,tgtpct,tchpct,utlpct,fantpts,pts100snp'
+# Full season or week by week stats
+scope = 1
+# Scoring type (Fanduel default)
+fs = 2
 
+headers = 'rank,id,player,pos,team,gms,snaps,snapsgm,snappct,rushpct,tgtpct,tchpct,utlpct,fantpts,pts100snp'
+sn = w = ew = p = None
 
 def scraper():
     browser = RoboBrowser(parser='lxml')
@@ -29,10 +35,22 @@ def scraper():
     # Open the previously hidden page
     for year in default_years:
         # Assign sn param here
+        sn = year
         for week in default_weeks:
             # Assign w and ew params here
+            w = week
+            ew = week
             for pos in default_pos:
                 # Assign p param here
+                p = pos
+                snap_count_url = 'https://fantasydata.com/nfl-stats/nfl-fantasy-football-snap-count-and-snaps-played.aspx?fs={}&stype=0&sn={}&scope={}&w={}&ew={}&s=&t=0&p={}&st=FantasyPointSnapPercentage&d=1&ls=&live=false&pid=true&minsnaps=4'.format(
+                    fs,
+                    sn,
+                    scope,
+                    w,
+                    ew,
+                    p
+                )
                 browser.open(snap_count_url)
                 content = browser.find_all('tr')
 
