e52f27b917e7a9ddf37a11b60178199128b0db0f
brockinit <brocklanoza@gmail.com>
2017-05-29 11:40:35 -1000

    Update version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='interscraped',
     packages=['interscraped'],
-    version='0.1.0',
+    version='0.1.2',
     description='A web scraper that obtains and parses NFL stats and fantasy data',
     author='Brock Lanoza',
     author_email='brock@sudokrew.com',
     url='https://github.com/brockinit/dfs-scrape-tools',
-    download_url='https://github.com/brockinit/dfs-scrape-tools/archive/0.1.0.tar.gz',
+    download_url='https://github.com/brockinit/dfs-scrape-tools/archive/0.1.2.tar.gz',
     keywords=['web-scrape', 'scraper', 'daily', 'fantasy', 'sports'],
     classifiers=[],
     install_requires=['bs4', 'requests', 'robobrowser']
